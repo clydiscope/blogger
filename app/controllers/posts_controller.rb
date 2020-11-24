766749b7c2eca04a77c95e809b67224f4e3ce840
clydiscope <clydiscope@gmail.com>
2020-11-24 14:55:10 -0500

    Adds creating and reading posts feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,31 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+    
+      flash[:notice] = "Post saved successfully"
+      redirect_to @post
+    else
+    
+      flash.now[:alert] = "There error an error creative the post. Please try again"
+      render :new
+    end
   end
 
   def edit
+  end
+  
+  def post_params
+    params.permit(:title, :body)
   end
 end
