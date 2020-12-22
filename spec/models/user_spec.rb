90e7b3a6ff5086c4d12041dcd16370930d8895af
clydiscope <clydiscope@gmail.com>
2020-12-22 15:15:03 -0500

    adds user formatting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Blogger User", email: "user@blogger.com", password: "password") }
+  let(:user) { User.create!(name: "blogger user", email: "user@blogger.com", password: "password") }
   
   #Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
@@ -19,7 +19,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
   
   describe "attributes" do
-    it "should have name and email attributes" do
+    it "should have captialized name and email attributes" do
       expect(user).to have_attributes(name: "Blogger User", email: "user@blogger.com")
     end
   end
