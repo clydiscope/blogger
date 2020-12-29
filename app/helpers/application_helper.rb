aa83dd82ffd1be8dff3c1dd40a94097446798536
clydiscope <clydiscope@gmail.com>
2020-12-29 13:18:47 -0500

    adds an avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,16 @@ module ApplicationHelper
 
 	def form_group_tag(errors, &block)
 	
-		css_class = 'form=group'
+		css_class = 'form-group'
 		css_class = 'has-error' if errors.any?
 		
 		content_tag :div, capture(&block), class: css_class
+	end
+
+	def avatar_url(user)
+		gravitar_id = Digest::MD5::hexdigest(user.email.downcase)
+		
+		"http://gravatar.com/avatar/#{gravitar_id}.png?s=48"
 	end
 
 end
