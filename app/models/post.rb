8097b984d8599752f1b5f48bfd76c1cc65434b57
clydiscope <clydiscope@gmail.com>
2021-01-05 09:58:16 -0500

    adds additional scopes to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,11 @@ class Post < ApplicationRecord
 	
 	default_scope { order('created_at DESC') }
 	
+	scope :ordered_by_title, -> { reorder(title: :asc) }
+	
+	scope :ordered_by_reverse_created_at, -> { reorder(created_at: :desc) }	 
+	
+	
 	validates :title, length: { minimum: 5 }, presence: true
 	validates :body, length: { minimum: 20 }, presence: true
 	validates :topic, presence: true
